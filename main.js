eeb2555e26f023c537dcb50ba44c8bcf3d0030ea
JanLan <idiomasjanlan@gmail.com>
2024-10-29 15:57:04 -0300

    probando desafio

---
+++ main.js
@@ -0,0 +1,9 @@
+const mundiales = [1930, 1934, 1938, 1950, 1954, 1958, 1962, 1966, 1970, 1974, 1978, 1982, 1986, 1990, 1994, 1998, 2002, 2006, 2010, 2014, 2018, 2022];
+
+let eleccion = Number(prompt("Ingresa tu año de nacimiento"));
+
+const mundialesVividos = mundiales.filter(mundial => mundial >= eleccion);
+
+alert("Has vivido todos estos mundiales: " +`${mundialesVividos}` ); 
+
+alert(`En total ${mundialesVividos.length} mundiales 😮.`);
